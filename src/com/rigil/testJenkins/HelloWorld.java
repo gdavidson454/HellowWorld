8333a6e5ab6f86c94cae648956fcb4f6b9e8d924
john qian <john.q@rigil.com>
2016-12-13 09:49:02 -0500

    asaa
    aa

--- src/com/rigil/testJenkins/HelloWorld.java
+++ src/com/rigil/testJenkins/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	  public static void main(String[] args) {
 	      // Prints "Hello, World" in the terminal window.
 		  // Just for testing to add this line.
+		  // added second line
 	      System.out.println("Hello, World");
 	   }
 }
