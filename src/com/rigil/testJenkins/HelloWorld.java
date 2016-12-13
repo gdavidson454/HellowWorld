80dd34ea8abbd2243e9df3790e44799334092e78
john qian <john.q@rigil.com>
2016-12-13 09:48:26 -0500

    Changed one line
    234

---
+++ src/com/rigil/testJenkins/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.rigil.testJenkins;
+
+public class HelloWorld {
+	  public static void main(String[] args) {
+	      // Prints "Hello, World" in the terminal window.
+		  // Just for testing to add this line.
+	      System.out.println("Hello, World");
+	   }
+}
