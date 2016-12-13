0825bad28d1549f3666aba1a95d38627da404755
john qian <john.q@rigil.com>
2016-12-13 15:02:32 -0500

    triger the new build ?

--- src/com/rigil/testJenkins/HelloWorld.java
+++ src/com/rigil/testJenkins/HelloWorld.java
@@ -5,7 +5,9 @@ public class HelloWorld {
 	      // Prints "Hello, World" in the terminal window.
 		  // Just for testing to add this line.
 		  // added second line
-		  // added third line for history
+		  // added third line for history added this line to trigger new build 
+		  // added this line to trigger new build 
+		 // added this line to trigger new build 
 	      System.out.println("Hello, World");
 	   }
 }
